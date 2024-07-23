2beb2f57d55058cca4f4940fd41fdd449c419212
daryll-macforce <134326322+daryll-macforce@users.noreply.github.com>
2024-07-23 16:42:13 -0700

    Update LaunchTuneRemote.ps1

--- LaunchTuneRemote.ps1
+++ LaunchTuneRemote.ps1
@@ -1,42 +1,12 @@
 # Load Windows Forms assembly for message boxes
 Add-Type -AssemblyName System.Windows.Forms
 
-function Manage-SystemRestore {
-    $systemRestoreEnabled = (Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SystemRestore" -Name "RPSessionInterval").RPSessionInterval -ne 0
-    
-    if (-not $systemRestoreEnabled) {
-        $enablePrompt = [System.Windows.Forms.MessageBox]::Show(
-            "System Restore is currently disabled. Would you like to enable it?",
-            "Enable System Restore",
-            [System.Windows.Forms.MessageBoxButtons]::YesNo,
-            [System.Windows.Forms.MessageBoxIcon]::Question
-        )
-        
-        if ($enablePrompt -eq 'Yes') {
-            try {
-                Enable-ComputerRestore -Drive "C:\"
-                [System.Windows.Forms.MessageBox]::Show("System Restore has been enabled.", "Success", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Information)
-                $systemRestoreEnabled = $true
-            }
-            catch {
-                [System.Windows.Forms.MessageBox]::Show("Failed to enable System Restore. Continuing without creating a restore point.", "Error", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Error)
-                return
-            }
-        }
-        else {
-            [System.Windows.Forms.MessageBox]::Show("System Restore will remain disabled. Continuing without creating a restore point.", "Information", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Information)
-            return
-        }
+function Launch-SystemRestore {
+    try {
+        Start-Process "rstrui.exe"
     }
-    
-    if ($systemRestoreEnabled) {
-        try {
-            Checkpoint-Computer -Description "Before installing/uninstalling software" -RestorePointType "MODIFY_SETTINGS"
-            [System.Windows.Forms.MessageBox]::Show("Restore point created successfully.", "Restore Point", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Information)
-        }
-        catch {
-            [System.Windows.Forms.MessageBox]::Show("Failed to create restore point. Continuing without creating one.", "Warning", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Warning)
-        }
+    catch {
+        [System.Windows.Forms.MessageBox]::Show("Failed to launch System Restore. Please run it manually from the Control Panel.", "Error", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Error)
     }
 }
 
@@ -59,6 +29,18 @@ if (!(Get-Command choco.exe -ErrorAction SilentlyContinue)) {
 $glaryInstalled = Is-Installed "Glary Utilities"
 $shutupInstalled = Is-Installed "O&O ShutUp10"
 
+$restorePrompt = [System.Windows.Forms.MessageBox]::Show(
+    "Do you want to create a restore point before making changes?",
+    "Create Restore Point",
+    [System.Windows.Forms.MessageBoxButtons]::YesNo,
+    [System.Windows.Forms.MessageBoxIcon]::Question
+)
+
+if ($restorePrompt -eq 'Yes') {
+    Launch-SystemRestore
+    [System.Windows.Forms.MessageBox]::Show("Please create a restore point in the opened window before continuing.", "Create Restore Point", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Information)
+}
+
 if ($glaryInstalled -or $shutupInstalled) {
     $uninstallPrompt = [System.Windows.Forms.MessageBox]::Show(
         "Glary Utilities and/or O&O ShutUp10 are already installed. Do you want to uninstall them?",
@@ -68,8 +50,6 @@ if ($glaryInstalled -or $shutupInstalled) {
     )
 
     if ($uninstallPrompt -eq 'Yes') {
-        Manage-SystemRestore
-
         # Uninstall Glary Utilities if installed
         if ($glaryInstalled) {
             choco uninstall glaryutilities-free -y
@@ -91,8 +71,6 @@ if ($glaryInstalled -or $shutupInstalled) {
     )
 
     if ($installPrompt -eq 'Yes') {
-        Manage-SystemRestore
-
         # Install Glary Utilities and O&O ShutUp10
         choco install glaryutilities-free -y
         choco install shutup10 -y
